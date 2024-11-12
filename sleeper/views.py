de1d02682427d0a860a70b8677ea3db7768f028c
Simon Gohl <sagohl@gmail.com>
2024-11-11 22:44:37 -0800

    impl champion api logic

--- sleeper/views.py
+++ sleeper/views.py
@@ -15,11 +15,26 @@ from sleeper.models import SleeperLeagueRulebook
 
 class SleeperLeagueChampionAPI(APIView):
     def get(self, request, league_id: str) -> Response:
-        # Add logic to get league champ
-        data = sleeper_api.get_league(league_id)
-        if "error" in data:
-            return Response(data, status=status.HTTP_400_BAD_REQUEST)
-        return Response(data, status=status.HTTP_200_OK)
+        # Get league data the contains the latest winner roster id
+        league_data = sleeper_api.get_league(league_id)
+        latest_winner_roster_id = league_data.get("metadata", {}).get("latest_league_winner_roster_id")
+        
+        # From the league rosters we can check againts the roster id to get the owner id
+        rosters = sleeper_api.get_league_rosters(league_id)
+        champ_id = None
+        for roster in rosters:
+            if roster["roster_id"] == int(latest_winner_roster_id):
+                champ_id = roster["owner_id"]
+
+        # Finally we can grab the user data we want to display from the league users
+        users = sleeper_api.get_league_users()
+        champ_data = None
+        for user in users:
+            if user["user_id"] == champ_id:
+                champ_data = roster["owner_id"]
+
+        return Response(champ_data, status=status.HTTP_200_OK)
+
     
 class SleeperTestAPI(APIView):
     def get(self, request) -> Response:
