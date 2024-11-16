941d80c5fd01fa137fba87d6ca049a6a9d06bdf1
Simon Gohl <sagohl@gmail.com>
2024-11-15 20:27:38 -0800

    HttpOnly auth update

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
+from django.conf import settings
+
 from django.shortcuts import render
+from django.contrib.auth import login
 
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import generics, status
@@ -17,21 +20,29 @@ class SignUpView(generics.CreateAPIView):
     serializer_class = SignUpSerializer
 
     def create(self, request, *args, **kwargs):
-        print(request)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         instance, token = AuthToken.objects.create(user)
 
-        return Response({
+        response = Response({
             "message": "User sign up successfully",
             "user": {
                 "user_id": user.id,
                 "username": user.username,
                 "email": user.email,
             },
-            "token": token,
         }, status=status.HTTP_201_CREATED)
+
+        response.set_cookie(
+            'knox_token',
+            token,
+            max_age=settings.KNOX['TOKEN_TTL'].total_seconds(),
+            httponly=True,
+            secure=settings.SESSION_COOKIE_SECURE,
+            samesite='Strict'
+        )
+        return response
 
 
 class LoginView(KnoxLoginView):
@@ -42,15 +53,32 @@ class LoginView(KnoxLoginView):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid(raise_exception=True):
             user = serializer.validated_data['user']
-            return super().post(request, format=None)
+            response = super().post(request, format=None)
+            token = response.data['token']
+            response.delete_cookie('knox_token')
+            response.set_cookie(
+                'knox_token',
+                token,
+                max_age=settings.KNOX['TOKEN_TTL'].total_seconds(),
+                httponly=True,
+                secure=settings.SESSION_COOKIE_SECURE,
+                samesite='Strict'
+            )
+            del response.data['token']
+            return response
 
 
 class LogoutView(KnoxLogoutView):
-    pass
+    def post(self, request, format=None):
+        response = super().post(request, format=None)
+        response.delete_cookie('knox_token')
+        return response
+
 
 class LogoutAllView(KnoxLogoutAllView):
     pass
   
+
 class AuthStatusView(APIView):
     permission_classes = [IsAuthenticated]
 
