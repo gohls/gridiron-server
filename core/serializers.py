61a5ab9e81649462c89fd484ba985f2cdfd1dfd9
Simon Gohl <sagohl@gmail.com>
2024-11-20 16:55:45 -0800

    remove unneeded create method

--- core/serializers.py
+++ core/serializers.py
@@ -18,14 +18,6 @@ class SignUpSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        user = PlatformUser.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        return user
-
 
 class SignInSerializer(serializers.Serializer):
     username = serializers.CharField()
