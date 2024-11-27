76c8fe0f6c86bc49ec08ffb48d8bc2215abda65f
Simon Gohl <sagohl@gmail.com>
2024-11-27 15:34:38 -0800

    register PlatformUser in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from core.models import PlatformUser  # Import your custom user model
+
+admin.site.register(PlatformUser, UserAdmin)
